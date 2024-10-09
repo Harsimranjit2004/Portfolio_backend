b1a0a97f091df2e400381384b935ab8f00a2bcf2
Harsimranjit-singh <135201698+Harsimranjit2004@users.noreply.github.com>
2024-10-08 20:28:15 -0400

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,7 @@
 const corsOptions = {
-  origin: "https://harsimranjitsingh.onrender.com", // Allow access from any origin
+//  origin: "https://harsimranjitsingh.onrender.com", // Allow access from any origin
   // origin: "http://localhost:5173",
+  origin: "https://harsimranjit2004.vercel.app"
   credentials: true,
   optionsSuccessStatus: 200,
 };
